7dd4ed57596fe62e0fee9a0b09ab7c48c02965f8
Jacek Spławski <splawski.j@gmail.com>
2014-09-21 21:42:39 +0200

    Add grunt-contrib-imagemin module, change grunt-notify confuguration and fix some SASS files.

--- Gruntfile.js
+++ Gruntfile.js
@@ -14,15 +14,37 @@ module.exports = function(grunt) {
           'tmp/style.css': 'sass/style.sass'
         }
       }
+    },
+    imagemin: {
+      dynamic: {
+        files: [{
+          expand: true,
+          src: ['img/*.{png,jpg,gif}'],
+          dest: 'prod'
+        }]
+      }
+    },
+    notify: {
+      sass: {
+        options: {
+          message: 'SASS and Uglify finished running.'
+        }
+      },
+      imagemin: {
+        options: {
+          message: 'Minify images finished running.'
+        }
+      }
     }
   });
 
   // Load the plugin that provides the "jshint" task.
   grunt.loadNpmTasks('grunt-contrib-jshint');
   grunt.loadNpmTasks('grunt-contrib-sass');
+  grunt.loadNpmTasks('grunt-contrib-imagemin');
   grunt.loadNpmTasks('grunt-notify');
 
   // Default task(s).
-  grunt.registerTask('default', ['jshint', 'sass']);
+  grunt.registerTask('default', ['jshint', 'sass', 'imagemin', 'notify']);
 
 };
